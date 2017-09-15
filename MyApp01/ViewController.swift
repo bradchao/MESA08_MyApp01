a158681694988c6039c9d767dfa1b43b88b51eae
Brad <brad@brad.tw>
2017-09-15 15:58:56 +0800

    2017091502

--- MyApp01/ViewController.swift
+++ MyApp01/ViewController.swift
@@ -17,12 +17,33 @@ class ViewController: UIViewController {
         myLabel.text = createLottery()
     }
     
+    @IBAction func showDialog(_ sender: Any) {
+        sDialog()
+    }
+    
+    private func sDialog(){
+        let alert = UIAlertController(title: "Welcome", message: "恭喜老爺", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {
+            (a) ->Void in
+            print("Press OK")
+        })
+        alert.addAction(action)
+        present(alert, animated: true, completion: {
+            print("Show Dialog")
+        })
+    }
     private func createLottery() -> String {
         var set:Set<Int> = []
         while set.count < 6 {
             set.insert(Int(arc4random_uniform(49)+1))
         }
-        return set.description
+        
+        var ret = ""
+        for lottery in set {
+            ret += "\(lottery) "
+        }
+        
+        return ret
     }
     
 }
